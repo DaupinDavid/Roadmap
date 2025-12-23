f3fb67d84405f3c2f3433fa585c5dd7bf850f9c6
DaupinDavid <david_945@live.fr>
2025-12-23 04:46:25 +0100

    feat: casino niveau 1

---
+++ casino_lvl1.py
@@ -0,0 +1,40 @@
+# Importation de la bibliothèque pour gérer le hasard
+import random
+
+# Fonction simulant le tirage d'une carte (entre 1 et 10)
+def tirer_carte():
+    nombre = random.randint(1, 10)
+    return nombre
+
+# Configuration initiale du jeu
+nom = "Joueur"
+montant = 100
+montant = int(montant)
+partie_jouées = 0
+
+# Simulation des 10 parties
+print(f"Bonjour {nom}, tu rentres avec {montant}€")
+while partie_jouées < 10 :
+    print(f"--- Partie Numéro {partie_jouées + 1} ---")
+    mon_score = tirer_carte()
+    print(f"Tu as tiré la carte n° {mon_score}")
+    score_croupier = tirer_carte()
+    print(f"Le croupier a tiré la carte n° {score_croupier}")
+
+    # Détermination du gagnant et mise à jour du montant
+    if mon_score > score_croupier:
+        montant = (montant + 10)
+        print("Félicitations! Tu as gagné!!") 
+    elif mon_score == score_croupier:
+        print("Egalité!")
+    else: 
+        montant = (montant - 10)
+        print("Dommage! Tu as perdu...")
+    partie_jouées = partie_jouées + 1
+    print(f"Tu dispose de {montant}€ ")
+
+    # Vérification si le joueur est ruiné
+    if montant <= 0:
+        print ("Tu es ruiné... Le Casino te remercie et te souhaites une agréable journée. A bientot!")
+        break
+print("Au revoir !")
