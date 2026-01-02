ceb23b94e293c3f51400d63fce77452e220dc587
DaupinDavid <david_945@live.fr>
2026-01-02 08:16:23 +0100

    feat: casino_lvl_1

--- Projet 1 - Casino/src/casino_lvl1.py
+++ Projet 1 - Casino/src/casino_lvl1.py
@@ -6,36 +6,60 @@ def tirer_carte():
     nombre = random.randint(1, 10)
     return nombre
 
+#Fonction principale du jeu
+def jouer_partie(montant, mise):
+    mon_score = tirer_carte()
+    score_croupier = tirer_carte()
+    if mon_score > score_croupier:
+        montant_partie = round(mise, 2)
+        msg = f"Félicitations! Tu as gagné {montant_partie:.2f}€!"
+    elif mon_score == score_croupier:
+        montant_partie = -round(mise / 2, 2)
+        msg = f"Egalité. Ici, l'égalité te fais perdre {abs(montant_partie):.2f}€, la moitié de ta mise... "
+    else: 
+        montant_partie = -round(mise, 2)
+        msg = f"Dommage... Tu as perdu {abs(montant_partie):.2f}€."
+    montant += montant_partie
+    montant = round(montant, 2)  
+    return montant, msg, mon_score, score_croupier
+    
+
 # Configuration initiale du jeu
-nom = "Joueur"
-montant = 100
-montant = int(montant)
-partie_jouées = 0
+nom = input("Bienvenue au Casino! Quel est ton nom? ")
+while True: # Boucle pour s'assurer que le montant est un entier positif
+    try:
+        montant = float(input(f"Salut {nom}! Combien souhaites-tu apporter au Casino aujourd'hui? (€) "))
+        if montant > 0:
+            break
+        else:
+            print("Veuillez entrer un montant au moins égal à 1.")
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+print(f"Félicitations {nom}! Tu démarres avec {montant:.2f}€.")
+montant_depart = montant
+mise = round(montant / 10, 2)  # Calcul de la mise par partie
+print(f"Chaque partie coûtera {mise:.2f}€.")
+parties_jouees = 0
 
 # Simulation des 10 parties
-print(f"Bonjour {nom}, tu rentres avec {montant}€")
-while partie_jouées < 10 :
-    print(f"--- Partie Numéro {partie_jouées + 1} ---")
-    mon_score = tirer_carte()
-    print(f"Tu as tiré la carte n° {mon_score}")
-    score_croupier = tirer_carte()
-    print(f"Le croupier a tiré la carte n° {score_croupier}")
-
-    # Détermination du gagnant et mise à jour du montant
-    if mon_score > score_croupier:
-        montant = (montant + 10)
-        print(f"Félicitations! Tu as gagné 10€! | Score Croupier: {score_croupier} | Ton Score: {mon_score}") 
-    elif mon_score == score_croupier:
-        montant = (montant - 5)
-        print(f"Egalité. Ici, l'égalité te fais perdre la moitié de ta mise... | Score Croupier: {score_croupier} | Ton Score: {mon_score}")
-    else: 
-        montant = (montant - 10)
-        print(f"Dommage... Tu as perdu 10€. | Score Croupier: {score_croupier} | Ton Score: {mon_score}")
-    partie_jouées = partie_jouées + 1
-    print(f"Tu dispose de {montant}€ ")
+print(f"Bonjour {nom}, tu rentres avec {montant:.2f}€. Il y aura 10 parties et chaque partie coûte {mise:.2f}€. Bonne chance!")
+while parties_jouees < 10 :
+    print(f"--- Partie Numéro {parties_jouees + 1} ---")
+    montant, msg, mon_score, score_croupier = jouer_partie(montant, mise)
+    print(f" Carte Croupier n° {score_croupier}")
+    print(f" Carte {nom} n° {mon_score}")
+    print(msg)
+    parties_jouees += 1
+    print(f"Tu dispose de {montant:.2f}€ ")
 
     # Vérification si le joueur est ruiné
     if montant <= 0:
-        print ("Tu es ruiné... Le Casino te remercie et te souhaites une agréable journée. A bientot!")
+        print ("Tu es ruiné... Le Casino te remercie et te souhaites une agréable journée. A bientôt!")
         break
+gain_net = montant - montant_depart
+roi = (gain_net / montant_depart)*100 if montant_depart != 0 else 0
+if montant > 0:
+    print(f"Fin du jeu! Tu repars avec {montant:.2f}€. Ton gain net est de {gain_net:.2f}€. ")
+    print(f"Ton retour sur investissement est de {roi:.2f}% par rapport à ta mise initiale.")
+
 print("Au revoir !")
